060b45d075d60a31ecaccd7c2f539eb957ee8f06
Chen Roth <rothchen@gmail.com>
2020-08-06 01:02:24 +0300

    implement all API endpoints using mongo

--- src/collections/user.ts
+++ src/collections/user.ts
@@ -1,19 +1,16 @@
-import { model, Schema, Document, Model } from 'mongoose';
-import { TodoSchema, ITodo } from './todo';
+import { model, Schema, Document, Model, Types } from 'mongoose';
+import { TodoSchema, ITodo, Todo } from './todo';
 import { hash, compare } from 'bcrypt';
 
-// this interface just the data of the user
+// this interface represents a single user instance
 interface IUser extends Document {
     username: string;
     email: string;
     password: string;
     todos: ITodo[];
-}
-
-// this interface extends the data of the user and adds behavior (functions) to it
-export interface IUserModel extends Model<IUser> {
-    login(username: string, password: string): Promise<IUser>;
-    register(username: string, password: string, email: string): Promise<string>;
+    createTodo(details: Pick<ITodo, 'description' | 'date'>): Promise<ITodo>;
+    toggleTodo(todoId: string): Promise<void>;
+    deleteTodo(todoId: string): Promise<void>;
 }
 
 const UserSchema = new Schema<IUser>({
@@ -34,6 +31,14 @@ UserSchema.path('email').validate((email: string) => {
     return emailRegex.test(email);
 }, 'Email must be a valid email address');
 
+
+// this interface represents the whole collection - login and register are operations done on the collection, before we have a specific user
+export interface IUserModel extends Model<IUser> {
+    login(username: string, password: string): Promise<IUser>;
+    register(username: string, password: string, email: string): Promise<string>;
+}
+
+// we use UserSchema.statics to implement IUserModel ----------
 
 // this is the implementation of IUserModel.login
 UserSchema.statics.login = async (username: string, password: string): Promise<IUser> => {
@@ -60,5 +65,45 @@ UserSchema.statics.register = async (username: string, password: string, email: 
     return userId;
 }
 
+//  --------------------------
+
+// we use UserSchema.methods to implement IUser behavior (methods) -----------
+
+// the createTodo method is specific to a user, so we use UserSchema.methods NOT UserSchema.statics
+UserSchema.methods.createTodo = async function ({ date, description }): Promise<ITodo> {
+    const todo = new Todo({ date, description });
+    this.todos.push(todo);
+    //  we don't want to validate any existing fields, such as the username etc, 
+    // so we pass `validateModifiedOnly: true` to only validate what's changed (the todos array)
+    await this.save({ validateModifiedOnly: true });
+    return this.todos[this.todos.length - 1];
+}
+
+UserSchema.methods.toggleTodo = async function (todoId: string): Promise<void> {
+    // we look for the specific todo by comparing the todoId (but remember it's an ObjectID, so we have to use Types.ObjectId wrapper)
+    const index = this.todos.findIndex(t => Types.ObjectId(todoId).equals(t._id));
+    if (index === -1) {
+        throw new Error("todo doesn't exist");
+    }
+    // if we found the todo, we update its isComplete field
+    this.todos[index].isComplete = !this.todos[index].isComplete;
+    // and save the document
+    await this.save({ validateModifiedOnly: true });
+}
+
+
+UserSchema.methods.deleteTodo = async function (todoId: string): Promise<void> {
+    // we look for the specific todo by comparing the todoId (but remember it's an ObjectID, so we have to use Types.ObjectId wrapper)
+    const index = this.todos.findIndex(t => Types.ObjectId(todoId).equals(t._id));
+    if (index === -1) {
+        throw new Error("todo doesn't exist");
+    }
+    // if we found this todo, we splice it out
+    this.todos.splice(index, 1);
+    //  and then save the modified document
+    await this.save({ validateModifiedOnly: true });
+}
+
+//  ------------------------
 
 export const User = model<IUser, IUserModel>('User', UserSchema);
--- src/routers/todosRouter.ts
+++ src/routers/todosRouter.ts
@@ -1,7 +1,6 @@
 import { Router } from 'express';
-import { getTodos, createTodo, deleteTodo, toggleComplete } from '../db/todosQueries';
 import { User } from '../collections';
-import { TodoSchema, Todo } from '../collections/todo';
+import { Todo } from '../collections/todo';
 
 export const todosRouter = Router();
 
@@ -14,35 +13,35 @@ todosRouter.get('/', async (req, res) => {
 todosRouter.post('/', async (req, res) => {
     const { userId } = (req as any).user;
     const { description, date } = req.body;
-    const todo = new Todo({ description, date });
-    const user = await User.findByIdAndUpdate(userId, { $push: { todos: todo } }, { new: true }).select('todos').exec();
-    const todoFromDb = user!.todos[user!.todos.length - 1];
-    res.send({ todo: todoFromDb });
+    const user = await User.findById(userId);
+    try {
+        const todo = await user?.createTodo(new Todo({ description, date }));
+        res.send({ todo });
+    } catch (e) {
+        res.status(400).send(e.message);
+    }
 });
 
 todosRouter.delete('/:todoId', async (req, res) => {
     const { userId } = (req as any).user;
     const { todoId } = req.params;
-    const isDeleted = await User.updateOne({ _id: userId }, { $pull: { todos: { _id: todoId } } });
-
-    if (isDeleted.ok === 1) {
+    const user = await User.findById(userId);
+    try {
+        await user?.deleteTodo(todoId);
         res.send('ok');
-    } else {
-        res.status(404).send("todo doesn't exist");
+    } catch (e) {
+        res.status(404).send(e.message);
     }
 });
 
 todosRouter.put('/:todoId/toggle', async (req, res) => {
     const { userId } = (req as any).user;
     const { todoId } = req.params;
-    const todoIdNum = Number(todoId);
-    if (isNaN(todoIdNum)) {
-        return res.send(400).send('id must be a number!');
-    }
-    const isSuccess = await toggleComplete(todoIdNum, userId);
-    if (isSuccess) {
+    const user = await User.findById(userId);
+    try {
+        await user?.toggleTodo(todoId);
         res.send('ok');
-    } else {
-        res.status(404).send("todo doesn't exist");
+    } catch (e) {
+        res.status(404).send(e.message);
     }
 });
