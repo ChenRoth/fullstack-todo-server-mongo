1531a5bf8e80fef75ad656fcc4da115033707213
Chen Roth <rothchen@gmail.com>
2020-08-05 23:44:16 +0300

    move register/login logic to User model

--- src/collections/user.ts
+++ src/collections/user.ts
@@ -1,11 +1,19 @@
-import { model, Schema, Document } from 'mongoose';
+import { model, Schema, Document, Model } from 'mongoose';
 import { TodoSchema, ITodo } from './todo';
+import { hash, compare } from 'bcrypt';
 
+// this interface just the data of the user
 interface IUser extends Document {
     username: string;
     email: string;
     password: string;
     todos: ITodo[];
+}
+
+// this interface extends the data of the user and adds behavior (functions) to it
+export interface IUserModel extends Model<IUser> {
+    login(username: string, password: string): Promise<IUser>;
+    register(username: string, password: string, email: string): Promise<string>;
 }
 
 const UserSchema = new Schema<IUser>({
@@ -23,7 +31,34 @@ UserSchema.path('username').validate(async (username: string) => {
 
 UserSchema.path('email').validate((email: string) => {
     const emailRegex = /^([\w-\.]+@([\w-]+\.)+[\w-]{2,4})?$/;
-    return emailRegex.test(email); // Assuming email has a text attribute
-}, 'The e-mail field cannot be empty.')
+    return emailRegex.test(email);
+}, 'Email must be a valid email address');
 
-export const User = model<IUser>('User', UserSchema);
+
+// this is the implementation of IUserModel.login
+UserSchema.statics.login = async (username: string, password: string): Promise<IUser> => {
+    const user = await User.findOne({ username }).select(['_id', 'password']).exec();
+    if (!user) {
+        throw new Error("username or password don't match");
+    }
+    const isPasswordCorrect = await compare(password, user.password);
+    if (!isPasswordCorrect) {
+        throw new Error("username or password don't match");
+    }
+    return user;
+}
+
+// this is the implementation of IUserModel.register
+UserSchema.statics.register = async (username: string, password: string, email: string): Promise<string> => {
+    const hashedPassword = await hash(password, 10)
+    const user = new User({
+        username,
+        password: hashedPassword,
+        email
+    });
+    const { _id: userId } = await user.save();
+    return userId;
+}
+
+
+export const User = model<IUser, IUserModel>('User', UserSchema);
--- src/routers/usersRouter.ts
+++ src/routers/usersRouter.ts
@@ -1,4 +1,3 @@
-import { hash, compare } from 'bcrypt';
 import { Router } from 'express';
 import jwt from 'jsonwebtoken';
 import { User } from '../collections';
@@ -9,34 +8,26 @@ const usersRouter = Router();
 
 usersRouter.post('/register', async (req, res) => {
     const { username, password, email } = req.body;
-    const hashedPassword = await hash(password, 10)
     try {
-        const user = new User({
-            username,
-            password: hashedPassword,
-            email
-        });
-        const { _id: userId } = await user.save();
+        const userId = await User.register(username, password, email);
         const token = jwt.sign({ userId }, JWT_SECRET);
         res.send({ token });
     } catch (e) {
-        res.status(403).send(e);
+        res.status(403).send(e.message);
     }
 });
 
 usersRouter.post('/login', async (req, res) => {
     const { username, password } = req.body;
-    const user = await User.findOne({ username }).select(['_id', 'password']).exec();
-    if (!user) {
-        return res.status(401).send("username or password don't match");
-    }
-    const isPasswordCorrect = await compare(password, user.password);
-    if (!isPasswordCorrect) {
-        return res.status(401).send("username or password don't match");
-    }
+    try {
+    const user = await User.login(username, password);
     const token = jwt.sign({ userId: user._id }, JWT_SECRET);
     res.send({ token });
+    } catch (e) {
+        res.status(401).send(e.message);
+    }
 });
 
 
 export { usersRouter };
+
